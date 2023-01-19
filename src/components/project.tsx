ba378d3fcd2bc8a711076f210ae62627839cc055
Carlo Bollini <14152206+carlobolla@users.noreply.github.com>
2023-01-19 16:23:09 +0100

    Updated site image, aligned to bottom images on the same row

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -4,8 +4,8 @@ import { Project } from "../classes";
 
 export const ProjectCard = (props: Project) => {
   return(
-    <div className="col-6 justify-content-center d-flex flex-column">
-      <img data-bs-toggle="modal" data-bs-target={'#' + props.text.replace(" ", "")} className="border border-2 img-fluid" src={props.imgLink} alt="Project" />
+    <div className="col-6 justify-content-end d-flex flex-column">
+      <img style={{"width": "100%", "height": "auto"}} data-bs-toggle="modal" data-bs-target={'#' + props.text.replace(" ", "")} className="border border-2 img-fluid" src={props.imgLink} alt="Project" />
       <Button className='align-self-center mt-3 w-75' text={props.text} link={props.link}></Button>
       <div id={props.text.replace(" ", "")} className="modal fade mt-5" tabIndex={-1}>
         <div className="modal-dialog modal-xl">
